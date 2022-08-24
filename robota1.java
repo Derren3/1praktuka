2cc45f80c0e31fcc70c358de8e0d87aec5db8414
Derren3 <111859627+Derren3@users.noreply.github.com>
2022-08-24 12:24:43 +0300

    robota1

---
+++ robota1.java
@@ -0,0 +1,20 @@
+package com.yanok;
+
+import java.util.Scanner;
+
+public class robota1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Введіть 2 числа не більше 1000, що відповідають двом катетам трикутника: ");
+		Scanner sc = new Scanner(System.in);
+		int katet1 = sc.nextInt();
+		int katet2 = sc.nextInt();
+		
+		double suma = katet1*katet1 + katet2*katet2;
+		suma = Math.sqrt(suma);
+		System.out.println("Гіпотенуза трикудника дорівнює = " + suma);
+
+	}
+
+}
